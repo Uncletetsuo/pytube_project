5178c040bcf4080c07c24299ec8fb4499f1bd31a
Fernando Castellanos <fer.cst96@gmail.com>
2023-06-09 20:09:44 -0600

    audiosegment file choice is still missing

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from pytube import YouTube
+from pydub import AudioSegment
 
-def Download(link):
-    youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
+prompt = input('Deja aquí el enlace de YouTube que se va a descargar -->')
+
+def Download(URL):    
     try:
-        youtubeObject.download()
+        video = YouTube(URL)
+        downloadvid = video.streams.filter(only_audio=True).first()
+        vid = downloadvid.download()
+        audio = AudioSegment.from_file(vid)
+        audio.export(f'{}.mp3', format='mp3')
+        print("La descarga se completó exitosamente.")
     except:
-        print("An error has occurred")
-    print("Download is completed successfully")
+        print("Ha ocurrido un error durante la descarga.")
 
+Download(prompt)
 
-link = input("Enter the YouTube video URL: ")
-Download(link)

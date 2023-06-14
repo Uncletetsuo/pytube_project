7c575289b538fade2e9f3f2e63ef8a5c4918e338
Fernando Castellanos <fer.cst96@gmail.com>
2023-06-13 18:32:30 -0600

    removed the original mp4 and there is now the possibility to choose between 3 other file extensions, working on having more

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from pytube import YouTube
 from pydub import AudioSegment
+import os
 
 prompt = input('Deja aquí el enlace de YouTube que se va a descargar -->')
 
 def Download(URL):    
     try:
+        output_format = input('Ingrese la extensión de salida deseada (por ejemplo, mp3, wav, Flac): ')
         video = YouTube(URL)
         downloadvid = video.streams.filter(only_audio=True).first()
         vid = downloadvid.download()
         audio = AudioSegment.from_file(vid)
-        audio.export(f'{}.mp3', format='mp3')
-        print("La descarga se completó exitosamente.")
+        audio.export(f'{video.title}.{output_format}', format=output_format)
+        print(f"La descarga se completó exitosamente. El archivo '{video.title}.{output_format}' se ha guardado.")
+        os.remove(vid)
     except:
         print("Ha ocurrido un error durante la descarga.")
 
 Download(prompt)
-
